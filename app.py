db08894d6a1c9778e780e105b80db72b4e781e36
KyrxsZ <nanatsuki711@gmail.com>
2026-09-25 16:44:20 +0700

    update port

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"])
+    app.run(host="0.0.0.0", port=5000, debug=False)
